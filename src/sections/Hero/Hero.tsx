c52449ea432a81d0e890c0135898e013145cd2e0
Ria0905 <115111805+Ria0905@users.noreply.github.com>
2026-05-26 15:21:56 +0200

    Fix build errors for deployment

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -1,15 +1,13 @@
-import { useContext } from "react";
 import "./Hero.Module.css";
-import { ThemeContext } from "../../context/ThemeContext";
+
 
 export default function Hero() {
-  
-
   return (
-    <section className="hero">
+    <section id="hero" className="hero">
       <div className="hero-content">
         <h1>Hi, I’m Ria 👋</h1>
         <h2>.NET & Frontend Developer</h2>
+        <p>Building modern web apps with React & .NET</p>
       </div>
     </section>
   );
